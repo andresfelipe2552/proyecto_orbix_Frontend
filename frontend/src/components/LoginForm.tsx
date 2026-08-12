96827bec414a76e0796f54c36e656f6886087350
juancito-10 <juannoriegasena@gmail.com>
2026-08-12 18:21:07 -0500

    feat: Conecxión de formulario de registro con endpoints

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,20 +1,36 @@
 import { useState } from "react";
 import "./LoginForm.css";
+import authService from "../services/auth.services";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    console.log(email);
-    console.log(password);
+    try {
+      const response = await authService.login(email, password);
+
+      console.log("Login exitoso:", response);
+
+      const token = response.data.token;
+
+      localStorage.setItem("token", token);
+
+      console.log("Token guardado");
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+    }
   };
+
   return (
     <form onSubmit={handleSubmit} className="form">
       <div className="form-group">
-        <h1 className="titulo">Orbix</h1>
+        <h1>Orbix</h1>
+
         <label htmlFor="correo">Correo electrónico</label>
+
         <input
           type="email"
           id="correo"
@@ -24,8 +40,10 @@ const LoginForm = () => {
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
+
       <div className="form-group">
         <label htmlFor="password">Contraseña</label>
+
         <input
           type="password"
           id="password"
@@ -35,8 +53,14 @@ const LoginForm = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <button type="submit" className="button-login">Iniciar sesión</button>
-      <a href="#" className="forgot-password" >¿Olvidaste la contraseña?</a>
+
+      <button type="submit" className="button-login">
+        Iniciar sesión
+      </button>
+
+      <a href="#" className="forgot-password">
+        ¿Olvidaste la contraseña?
+      </a>
     </form>
   );
 };
