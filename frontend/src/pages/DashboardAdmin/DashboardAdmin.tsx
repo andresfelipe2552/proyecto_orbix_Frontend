7031f022aede7864881d8c98336797d97cf8898d
juancito-10 <juannoriegasena@gmail.com>
2026-08-16 23:43:19 -0500

    feat: avance dashboard admin

--- frontend/src/pages/DashboardAdmin/DashboardAdmin.tsx
+++ frontend/src/pages/DashboardAdmin/DashboardAdmin.tsx
@@ -1,4 +1,6 @@
 import Sidebar from "../../components/dashboardAdmin/Sidebar";
+import CarsDatos from "../../components/dashboardAdmin/CarsDatos";
+import { Search, Bell, Download } from "lucide-react";
 import "./DashboardAdmin.css";
 
 const DashboardAdmin = () => {
@@ -6,6 +8,39 @@ const DashboardAdmin = () => {
     <main className="main">
       <Sidebar />
 
+      <div className="contenido-dashboard">
+        <div className="barra-superior">
+          <p>
+            <span className="orbix">Orbix</span> /{" "}
+            <span className="admin">Admin</span> /{" "}
+            <span className="dashboard-letra">Dashboard</span>
+          </p>
+
+          <div className="acciones-superiores">
+            <form className="buscar">
+              <Search size={20} />
+              <input type="text" placeholder="Buscar..." />
+            </form>
+            <div className="notifi">
+              <Bell size={20} />
+            </div>
+            <div className="usuario">VO</div>
+          </div>
+        </div>
+
+        <div className="encabezado-dashboard">
+          <div>
+            <h2>Dashboard</h2>
+            <p className="fecha-mes">Miércoles, 30 de julio de 2026</p>
+          </div>
+
+          <button className="button-exportar">
+            <Download size={20} />
+            Exportar reporte
+          </button>
+        </div>
+        <CarsDatos />
+      </div>
     </main>
   );
 };
